a877d85425884e3371b151bb7a723322ce57fb7f
Florian Le Frioux <florian@lefrioux.fr>
2016-12-11 12:50:19 +0100

    Finished games are now a thing

--- public/js/main.js
+++ public/js/main.js
@@ -10,12 +10,12 @@ var gamesList = new Vue({
     },
     created: function() {
         this.fetchData();
-        this.timer = setInterval(this.fetchData, 10000);
+        this.timer = setInterval(this.fetchData, 30000);
     },
     methods: {
         fetchData: function() {
             var self = this;
-            axios.get('/games/?total_bits_gte=1').then(function (response) {
+            axios.get('/games/?total_bits_gte=1&finished=false').then(function (response) {
                 self.gamesCheered = _.chain(response.data)
                     .sortBy(function(game) {
                         return game.priority;
@@ -32,30 +32,40 @@ var gamesList = new Vue({
     }
 });
 
-Vue.component('game-other-item', {
+Vue.component('game-uncheered-item', {
     props: ['game'],
     template: '<li>{{ game.id }} : {{ game.game_name }}</li>'
 });
 
 var gamesOtherList = new Vue({
-    el: '#game-other-list',
+    el: '#game-other-lists',
     data: {
         gamesOther: null,
+        gamesFinished: null,
         displayAllGames: false
     },
     created: function() {
         this.fetchData();
-        this.timer = setInterval(this.fetchData, 10000);
+        this.timer = setInterval(this.fetchData, 30000);
     },
     methods: {
         fetchData: function() {
             var self = this;
-            axios.get('/games/?total_bits=0&_sort=game_name').then(function (response) {
+            axios.get('/games/?total_bits=0&finished=false&_sort=game_name').then(function (response) {
                 self.gamesOther = response.data;
+            });
+
+            axios.get('/games/?finished=true&_sort=game_name').then(function (response) {
+                self.gamesFinished = response.data;
             });
         }
     },
     beforeDestroy() {
       clearIntervall(this.timer)
     }
-})
+});
+
+Vue.component('game-finished-item', {
+    props: ['game'],
+    template: '<li>{{ game.id }} : {{ game.game_name }}</li>'
+});
